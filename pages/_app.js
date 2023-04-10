b7f9c29b26f47c3fafe61681970a52b741bfa080
almanac <sarumaha_1@yahoo.com>
2023-04-10 14:45:32 +0700

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+import '../styles/customBootstrap.scss'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
+  
+  return <Component {...pageProps} />
+}
+
+export default MyApp
